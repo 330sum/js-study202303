8a935d179b922579093815fc06db6e63daf3e890
330sum <126743151+330sum@users.noreply.github.com>
2023-03-15 16:00:53 +0900

    js 1-3. 비교연산자

---
+++ 1. 자바스크립트 기초/3. 비교연산자.js
@@ -0,0 +1,34 @@
+
+var a =5;
+var b ='5';
+
+console.log(a === b);
+console.log(a !== b);
+
+console.log('====================================');
+
+// == 비교는 결과 예측이 어려움
+console.log('0' === '');
+console.log(0 === '');
+console.log('0' === 0);
+console.log(false === 'false');
+console.log(false === '0');
+
+console.log('====================================');
+
+// 문자 대소비교
+console.log('ace' < 'ade');
+console.log('cobra' < 'zebra');
+console.log('cobra' < 'cobro');
+console.log('cobra' > 'cobrO');
+
+
+console.log('====================================');
+
+var money = 0;
+
+if (!money) {
+    console.log('저는 그지입니다.');
+} else {
+    console.log('저는 돈이 있습니다.');
+}
