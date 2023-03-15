3f2fd0c2202916aa5b034705ce2f2ead6aff98c8
330sum <126743151+330sum@users.noreply.github.com>
2023-03-15 16:02:50 +0900

    js quiz

---
+++ 1. 자바스크립트 기초/6. quiz1.js
@@ -0,0 +1,27 @@
+// x ~ y까지의 누적합
+
+var x = +prompt('첫번째 숫자를 입력!');
+var y = +prompt('두번째 숫자를 입력!');
+
+if (x > y) {
+    //swap
+    var t;
+    t = x;
+    x = y;
+    y = t;  
+}
+
+
+var total = 0; //초기값
+var n = x; 
+
+while (n <= y) { //끝값
+    //반복코드
+    total += n;
+
+    n++; //증감식
+}
+
+console.log(`${x} ~ ${y}까지의 누적합: ${total}`);
+
+
