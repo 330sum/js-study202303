02e24d69e526c98c303281745ff4c7f954a625e6
330sum <126743151+330sum@users.noreply.github.com>
2023-03-15 11:50:55 +0900

    js 기초

---
+++ 1. 자바스크립트 기초/1. 데이터타입.js
@@ -0,0 +1,43 @@
+
+// var sentence = '<ul>\n\t<li>\n\t\t<a>으이렇게적어야한다니</a>';
+
+var sentence = `
+<ul>
+    <li>
+        <a>헤헤헤</a>
+    </li>
+</ul>
+`;
+
+// console.log(sentence);
+
+var month = 4;
+var day = 5;
+var anniversary = '식목일';
+
+// var message = month + '월 ' 
+//                 + day +'일은 ' 
+//                 + anniversary + '입니다.';
+
+var message = `${month}월 ${day}일은 ${anniversary}입니다.`
+
+
+// console.log(message);
+
+
+//논리
+var flag = false;
+console.log(typeof flag);
+
+
+var userAge = 5;
+userAge = null;
+
+
+var poppy = {
+    name: '뽀삐',
+    age: 3,
+    kind: '진돗개'
+};
+
+poppy = null;
---
+++ hello.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 30;
+console.log(a*b);
+
+
+var num1 = 10;
+console.log(typeof num1);
+var num2 = 10.5;
+console.log(typeof num2);
