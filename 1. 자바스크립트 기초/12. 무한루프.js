bde678d2d16d6700df323c3c10101bbd6517a9cc
330sum <126743151+330sum@users.noreply.github.com>
2023-03-16 10:48:03 +0900

    js 1-11. 무한루프

---
+++ 1. 자바스크립트 기초/12. 무한루프.js
@@ -0,0 +1,17 @@
+// var n = 1;
+// for (;;) {
+//     console.log(n++);
+//     if (n === 10) break;
+// }
+// console.log('무한루프에서 나왔쮸?');
+
+
+
+var n = 1;
+while (true) {
+    console.log(n++);
+    if (n === 10) break;
+}
+console.log('무한루프에서 나왔쮸?');
+console.log('===========================');
+
