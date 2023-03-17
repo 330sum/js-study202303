d6b71b978c70623a5b5f7dfe7d2767d70e54258b
330sum <126743151+330sum@users.noreply.github.com>
2023-03-17 16:20:40 +0900

    js 2-2. shift, unshift

--- 2. 배열/2. 배열요소 추가, 삭제.js
+++ 2. 배열/2. 배열요소 추가, 삭제.js
@@ -11,7 +11,7 @@ console.log('------------------------------------------');
 // console.log(pets);
 // 배열 요소 추가 시, 위는 좋은 방법이 아님 (자바에서는 아예 안됨)
 
-// 아래는 성능적 측면에서 문제가a없는 방법 임
+// 아래는 성능적 측면에서 문제가 없는 방법 임
 pets.push('징징이');
 pets.push('어흥이');
 var newLength = pets.push('거부기', '콩콩이');
@@ -31,19 +31,29 @@ console.log('------------------------------------------');
 // pets.pop();
 // console.log(pets); //[]
 
-var removeTarget = pets.pop();
-console.log(removeTarget); //콩콩이
-var removeTarget = pets.pop();
-console.log(removeTarget); //거부기
-var removeTarget = pets.pop();
-console.log(removeTarget); //어흥이
-var removeTarget = pets.pop();
-console.log(removeTarget); //징징이
-var removeTarget = pets.pop();
-console.log(removeTarget); //쩝쩝이
-var removeTarget = pets.pop();
-console.log(removeTarget); //야옹이
-var removeTarget = pets.pop();
-console.log(removeTarget); //멍멍이
-var removeTarget = pets.pop();
-console.log(removeTarget); //undefined
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //콩콩이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //거부기
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //어흥이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //징징이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //쩝쩝이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //야옹이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //멍멍이
+// var removeTarget = pets.pop();
+// console.log(removeTarget); //undefined
+
+
+
+// shift() : 배열의 맨 첫번째 요소 제거
+pets.shift();
+console.log(pets);
+
+// unshift() : 배열에 매 첫번째 데이터 추가
+pets.unshift('짝짝이');
+console.log(pets);
