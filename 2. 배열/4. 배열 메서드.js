11dd0d54092b6119955db136b5da3f72a30caf21
330sum <126743151+330sum@users.noreply.github.com>
2023-03-17 18:46:01 +0900

    수정

--- 2. 배열/4. 배열 메서드.js
+++ 2. 배열/4. 배열 메서드.js
@@ -2,7 +2,7 @@ var foodList = ['닭꼬치', '볶음밥', '짜장면', '족발'];
 
 var count = 0;
 
-// indexOf : 배열의 특정 데이터가 몇번 인덱스에 있는지 알려 줌
+// indexOf() : 배열의 특정 데이터가 몇번 인덱스에 있는지 탐색
 var target = '짜장면';
 var index = foodList.indexOf(target);
 console.log(`찾은 인덱스 : ${index}`);
@@ -17,7 +17,7 @@ console.log(`찾은 인덱스 : ${index}`);
 // }
 // console.log(`찾은 인덱스 : ${count}`);
 
-
+console.log(('========================================================'));
 
 // slice() : 배열을 일정부분 잘라냄. 사본배열을 반환 (원본은 그대로 있음)
 foodList.push('오뎅', '순대국밥');
@@ -37,9 +37,9 @@ var sliceFoods3 = foodList.slice();
 console.log(sliceFoods3);
 
 
+console.log(('========================================================'));
 
 // reverse() : 배열 역순으로 배치, 원본이 변함
-console.log(('===================================='));
 
 var nums = [10, 20, 30, 40, 50];
 var copyNums = nums.slice();
@@ -49,12 +49,13 @@ copyNums.reverse();
 console.log(copyNums);
 
 // swap로 length/2 만큼 반복하기
+var reverseArray 
 
 
 
+console.log(('========================================================'));
 
 // concat() : 배열 2개를 연결 (사본배열 반환)
-console.log(('===================================='));
 
 var arr1 = [10, 20, 30];
 var arr2 = [100, 900, 700, 1000];
@@ -64,17 +65,18 @@ var concatedArr = arr1.concat(arr2);
 console.log(concatedArr);
 
 
+console.log(('========================================================'));
 
 // includes() : 배열의 특정 데이터가 존재하는지 확인
-console.log(('===================================='));
+
 
 var resultFlag = foodList.includes('닭꼬치');
 console.log(resultFlag); //true
 
 
+console.log(('========================================================'));
 
 // splice() : 배열의 특정요소 제거, 삽입 (성능면에서 좋지 않음) (원본 반영)
-console.log(('===================================='));
 
 console.log(foodList);
 
