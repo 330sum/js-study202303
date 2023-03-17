dc0e906b4b07833d1384eaafb92e3f55c722abb0
330sum <126743151+330sum@users.noreply.github.com>
2023-03-17 12:03:36 +0900

    js 2-2. 배열요소 추가, 삭제

---
+++ 2. 배열/2. 배열요소 추가, 삭제.js
@@ -0,0 +1,49 @@
+var pets = ['멍멍이', '야옹이', '쩝쩝이'];
+console.log(pets);
+
+
+console.log('------------------------------------------');
+
+// 맨 끝 데이터 추가
+
+// pets[3] = '징징이';
+// pets[pets.length] = '징징이';
+// console.log(pets);
+// 배열 요소 추가 시, 위는 좋은 방법이 아님 (자바에서는 아예 안됨)
+
+// 아래는 성능적 측면에서 문제가a없는 방법 임
+pets.push('징징이');
+pets.push('어흥이');
+var newLength = pets.push('거부기', '콩콩이');
+console.log(pets);
+console.log(newLength);
+
+console.log('------------------------------------------');
+
+//  맨 끝 데이터 삭제
+
+// pets.pop();
+// pets.pop();
+// pets.pop();
+// pets.pop();
+// pets.pop();
+// pets.pop();
+// pets.pop();
+// console.log(pets); //[]
+
+var removeTarget = pets.pop();
+console.log(removeTarget); //콩콩이
+var removeTarget = pets.pop();
+console.log(removeTarget); //거부기
+var removeTarget = pets.pop();
+console.log(removeTarget); //어흥이
+var removeTarget = pets.pop();
+console.log(removeTarget); //징징이
+var removeTarget = pets.pop();
+console.log(removeTarget); //쩝쩝이
+var removeTarget = pets.pop();
+console.log(removeTarget); //야옹이
+var removeTarget = pets.pop();
+console.log(removeTarget); //멍멍이
+var removeTarget = pets.pop();
+console.log(removeTarget); //undefined
